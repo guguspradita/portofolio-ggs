1bd960bbe305e8f7de358c6f008ec3148b995283
guguspradita <guguspradita19@gmail.com>
2024-01-26 14:39:28 +0700

    header, hero and about

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,16 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+     <div class="container">
+         <h3>Gugus Pradita</h3>
+         <p>💻 Enthusiastic about web development and network engineer, especially backend-related work.</p>
+         <div class="social-links">
+             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+             <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+             <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+         </div>
+         <div class="copyright">
+             &copy; Copyright <strong><span>Gugus Pradita</span></strong>. All Rights Reserved
+         </div>
+     </div>
+ </footer><!-- End Footer -->
